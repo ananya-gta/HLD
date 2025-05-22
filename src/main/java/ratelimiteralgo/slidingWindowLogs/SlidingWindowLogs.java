a37face8d5b55ea097431907ced254bae7b6e91d
ananya-gta <anannya.gta@gmail.com>
2025-05-22 16:57:22 +0530

    added sliding window logs algo

--- src/main/java/ratelimiteralgo/slidingWindowLogs/SlidingWindowLogs.java
+++ src/main/java/ratelimiteralgo/slidingWindowLogs/SlidingWindowLogs.java
@@ -1,4 +1,30 @@
 package main.java.ratelimiteralgo.slidingWindowLogs;
 
+import java.time.Instant;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SlidingWindowLogs {
+    private final long windowSizeInMilliseconds;
+    private  final int maxRequests;
+    private final Queue<Long> queue;
+
+    public SlidingWindowLogs(long windowSizeInMilliseconds, int maxRequests) {
+        this.windowSizeInMilliseconds = windowSizeInMilliseconds;
+        this.maxRequests = maxRequests;
+        this.queue = new LinkedList<>();
+    }
+
+    public synchronized boolean allowRequests() {
+        long now = Instant.now().toEpochMilli();
+
+        // remove timestamps that ae older than current window
+        while (!queue.isEmpty() && (now - queue.peek()) > windowSizeInMilliseconds) queue.poll();
+
+        if (queue.size() < maxRequests ) {
+            queue.offer(now);
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/ratelimiteralgo/slidingWindowLogs/SlidingWindowLogsTest.java
+++ src/main/java/ratelimiteralgo/slidingWindowLogs/SlidingWindowLogsTest.java
@@ -1,4 +1,18 @@
 package main.java.ratelimiteralgo.slidingWindowLogs;
 
+import java.time.Instant;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SlidingWindowLogsTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        SlidingWindowLogs limiter = new SlidingWindowLogs(10, 5); // 5 requests per 10 seconds
+
+        for (int i = 1; i <= 10; i++) {
+            boolean allowed = limiter.allowRequests();
+            System.out.println("Request " + i + ": " + (allowed ? "Allowed" : "Rejected"));
+            Thread.sleep(1000); // simulate 1 second between requests
+        }
+    }
 }
