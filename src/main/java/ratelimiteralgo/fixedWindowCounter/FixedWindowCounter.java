392027e4fbe8f9be75e1d8b0e6631e7452d7c44f
ananya-gta <anannya.gta@gmail.com>
2025-05-22 16:12:23 +0530

    added fixed window counter algo

--- src/main/java/ratelimiteralgo/fixedWindowCounter/FixedWindowCounter.java
+++ src/main/java/ratelimiteralgo/fixedWindowCounter/FixedWindowCounter.java
@@ -1,4 +1,35 @@
 package main.java.ratelimiteralgo.fixedWindowCounter;
 
+import java.time.Instant;
+
 public class FixedWindowCounter {
+
+    private final long limit;
+    private Instant window;
+    private long counter;
+
+    public FixedWindowCounter(long limit) {
+        this.limit = limit;
+        this.window = Instant.now();
+        this.counter = 0;
+    }
+
+    public synchronized boolean allowRequests(int windowSizeInSeconds) {
+        Instant now = Instant.now();
+        System.out.println("current window: " + window);
+        if (now.minusSeconds(windowSizeInSeconds).isBefore(window)) { // are we still inside the current n seconds time window ?
+            // if yes
+            if (counter < limit) { // check if counter of the current window is less than the limit of the window,
+                counter++; // also increase the counter
+                return true; // then allow request
+            } else {
+                return false; // don't allow request if counter exceeds limit
+            }
+        } else { // if we are not inside the current 1-minute time window
+            // start a new window
+            window = now;
+            counter = 1; // count the current request
+            return true; // allow the current request
+        }
+    }
 }
--- src/main/java/ratelimiteralgo/fixedWindowCounter/FixedWindowCounterTest.java
+++ src/main/java/ratelimiteralgo/fixedWindowCounter/FixedWindowCounterTest.java
@@ -1,4 +1,15 @@
 package main.java.ratelimiteralgo.fixedWindowCounter;
 
+import java.time.Instant;
+
 public class FixedWindowCounterTest {
+    public static void main(String[] args) throws InterruptedException {
+        FixedWindowCounter obj = new FixedWindowCounter(5); // Allow 5 requests per 60-second window
+
+        for (int i = 1; i <= 20; i++) {
+            boolean allowed = obj.allowRequests(10);
+            System.out.println(Instant.now() + "Request " + i + ": " + (allowed ? "Allowed" : "Rejected"));
+            Thread.sleep(1000); // Wait 1 second between requests to simulate real-time traffic
+        }
+    }
 }
